02ee00243854623bd751f9c4d7355671201a39e1
unknown <handesushanth@gmail.com>
2019-06-17 22:55:45 +0530

    Added SD_SPI file

---
+++ Prototype/TCAM_ECU_ESP32/SD_SPI.ino
@@ -0,0 +1,267 @@
+//#include <Arduino.h>
+//#include <ESP32CAN.h>
+//#include <CAN_config.h>
+//#include <WiFi.h>
+//#include <HTTPClient.h>
+//#include "FS.h"
+//#include "SD.h"
+//#include "SPI.h"
+//#include <CRC32.h>
+
+const char* ssid = "HANDE_WIFI";
+const char* password =  "3305725800";
+
+const char* filenamePath = "/NewFirmware.bin";
+const char* DownloadLink = "https://raw.githubusercontent.com/Auto-Coders/FOTA_TCAM_ESP32_Arduino/master/esp32can.bin";
+
+// create buffer for read
+uint8_t buff[512] = { 0 };
+
+//Call this only when Wifi is connected
+bool DownloadFile(void)
+{
+  uint32_t filelen;
+  HTTPClient http;
+  bool retval = false;
+  
+  http.begin(DownloadLink);
+  int httpCode = http.GET();                                        //Make the request
+
+  if (httpCode > 0)
+  { //Check for the returning code
+
+    // get lenght of document (is -1 when Server sends no Content-Length header)
+    int len = http.getSize();
+    filelen = len;
+
+    // get tcp stream
+    WiFiClient * stream = http.getStreamPtr();
+
+    deleteFile(SD, filenamePath);
+    File file = SD.open(filenamePath, FILE_WRITE);
+
+    // read all data from server
+    while (http.connected() && (len > 0 || len == -1))
+    {
+      // get available data size
+      size_t size = stream->available();
+
+      if (size)
+      {
+        // read up to 128 byte
+        int c = stream->readBytes(buff, ((size > sizeof(buff)) ? sizeof(buff) : size));
+
+        //Append to file
+        file.write((const uint8_t*) buff, c);
+        memset(buff, 0, sizeof(buff));
+
+        if (len > 0)
+        {
+          len -= c;
+        }
+      }
+    }
+    file.close();
+
+    retval = true;
+  }
+  else
+  {
+    Serial.println("Error on HTTP request");
+  }
+
+  http.end(); //Free the resources
+
+  return retval;
+}
+
+//Max bytes is 8
+bool TransferFile(fs::FS &fs, const char * path)
+{
+  //Last 4 bytes of the bin file is the crc
+  //Calculate a checksum one byte at a time.
+  CRC32 crc;
+  uint32_t ByteCounter = 0, i = 0;
+  uint8_t ReadBuffer[8] = {0};
+
+  Serial.printf("Reading file: %s\n", path);
+
+  File file = fs.open(path);
+
+  uint32_t flen = file.size();
+
+  //Ignoring last 4 bytes as it is CRC
+  flen = flen - 4;
+
+  if (!file)
+  {
+    Serial.println("Failed to open file for reading");
+    return false;
+  }
+
+  Serial.print("Read from file: \n");
+  while(flen)
+  {
+    ReadBuffer[i++] = file.read();
+
+    i = i%8;
+    
+    ByteCounter++;
+    if(ByteCounter % 8 == 0)
+    {
+      //Send via CAN
+      //Remove, added for debugging only
+//      Serial.printf("%2X %2X %2X %2X %2X %2X %2X %2X\n", ReadBuffer[0], ReadBuffer[1], ReadBuffer[2], ReadBuffer[3],
+//                                                        ReadBuffer[4], ReadBuffer[5], ReadBuffer[6], ReadBuffer[7]);
+    }
+
+    flen--;
+
+    //Leftover bytes
+    if((flen == 0)&&(ByteCounter % 8 != 0))
+    {
+      //Send via CAN
+      //Remove, added for debugging only
+//      Serial.printf("%2X %2X %2X %2X %2X %2X %2X %2X\n", ReadBuffer[0], ReadBuffer[1], ReadBuffer[2], ReadBuffer[3],
+//                                                        ReadBuffer[4], ReadBuffer[5], ReadBuffer[6], ReadBuffer[7]);
+    }
+  }
+
+  return true;
+}
+
+bool VerifyFile_CRC32(fs::FS &fs, const char * path)
+{
+  //Last 4 bytes of the bin file is the crc
+  // Calculate a checksum one byte at a time.
+  CRC32 crc;
+
+  Serial.printf("Reading file: %s\n", path);
+
+  File file = fs.open(path);
+
+  uint32_t flen = file.size();
+
+  //Ignoring last 4 bytes as it is CRC
+  flen = flen - 4;
+
+  if (!file)
+  {
+    Serial.println("Failed to open file for reading");
+    return false;
+  }
+
+  Serial.print("Read from file: \n");
+  while (flen)
+  {
+    uint8_t readbyte = file.read();
+    crc.update(readbyte);
+    flen--;
+  }
+
+  uint8_t ds[4] = {0};
+  uint32_t CRCinFile = 0;
+  Serial.printf("File size is %d\n", file.size());
+  if (file.seek((file.size() - 4)))
+  {
+    Serial.printf("Seek success\n");
+  }
+  else
+  {
+    Serial.printf("Seek fail\n");
+  }
+
+  Serial.printf("Current position is %d\n", file.position());
+
+  for (int i = 0; i < 4; i++)
+  {
+    ds[i] = file.read();
+    CRCinFile = (CRCinFile | (ds[i] << (8 * i))); //8*i
+  }
+
+  Serial.printf("CRC: %0.2X%0.2X%0.2X%0.2X\n", ds[3], ds[2], ds[1], ds[0]);
+
+  file.close();
+  // Once we have added all of the data, generate the final CRC32 checksum.
+  uint32_t CRCCalculated = crc.finalize();
+
+  if (CRCCalculated == CRCinFile)
+  {
+    Serial.printf("CRC is %X calculated is %X, File OK\n", CRCinFile, CRCCalculated);
+    return true;
+  }
+  else
+  {
+    Serial.printf("CRC is %X calculated is %X, File corrupted\n", CRCinFile, CRCCalculated);
+    return false;
+  }
+}
+
+
+
+bool VerifyDowloadedFile_CRC32(fs::FS &fs, const char * path)
+{
+  //Last 4 bytes of the bin file is the crc
+  // Calculate a checksum one byte at a time.
+  CRC32 crc;
+
+  Serial.printf("Reading file: %s\n", path);
+
+  File file = fs.open(path);
+
+  uint32_t flen = file.size();
+
+  //Ignoring last 4 bytes as it is CRC
+  flen = flen - 4;
+
+  if (!file)
+  {
+    Serial.println("Failed to open file for reading");
+    return false;
+  }
+
+  Serial.print("Read from file: \n");
+  while (flen)
+  {
+    uint8_t readbyte = file.read();
+    crc.update(readbyte);
+    flen--;
+  }
+
+  uint8_t ds[4] = {0};
+  uint32_t CRCinFile = 0;
+  Serial.printf("File size is %d\n", file.size());
+  if (file.seek((file.size() - 4)))
+  {
+    Serial.printf("Seek success\n");
+  }
+  else
+  {
+    Serial.printf("Seek fail\n");
+  }
+
+  Serial.printf("Current position is %d\n", file.position());
+
+  for (int i = 0; i < 4; i++)
+  {
+    ds[i] = file.read();
+    CRCinFile = (CRCinFile | (ds[i] << (8 * i))); //8*i
+  }
+
+  Serial.printf("CRC: %0.2X%0.2X%0.2X%0.2X\n", ds[3], ds[2], ds[1], ds[0]);
+
+  file.close();
+  // Once we have added all of the data, generate the final CRC32 checksum.
+  uint32_t CRCCalculated = crc.finalize();
+
+  if (CRCCalculated == CRCinFile)
+  {
+    Serial.printf("CRC is %X calculated is %X, File OK\n", CRCinFile, CRCCalculated);
+    return true;
+  }
+  else
+  {
+    Serial.printf("CRC is %X calculated is %X, File corrupted\n", CRCinFile, CRCCalculated);
+    return false;
+  }
+}
